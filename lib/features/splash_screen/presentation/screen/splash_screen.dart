e7a652aa78d4fb54d3c5265415cfb02baaac399e
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-09-30 20:13:58 +0300

    refactor splash Screen

--- lib/features/splash_screen/presentation/screen/splash_screen.dart
+++ lib/features/splash_screen/presentation/screen/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:mini_chat/core/constants/icons.dart';
 import 'package:mini_chat/features/home/presentation/screens/home.dart';
+import 'package:mini_chat/features/splash_screen/presentation/screen/splash_body.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -24,28 +23,8 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: SvgPicture.asset(
-                AppIcon.whatsApp,
-                height: MediaQuery.of(context).size.height * 0.2,
-                width: MediaQuery.of(context).size.width * 0.2,
-              ),
-            ),
-            const Text(
-              'Whats Up',
-              style: TextStyle(
-                color: Colors.green,
-              ),
-            ),
-          ],
-        ),
-      ),
+    return const Scaffold(
+      body: SplashBody(),
     );
   }
 }
---
+++ lib/features/splash_screen/presentation/widgets/whats_app_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Text buildTextSplash() {
+  return const Text(
+    'Whats Up',
+    style: TextStyle(
+      color: Colors.green,
+    ),
+  );
+}
