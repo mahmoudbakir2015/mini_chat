712aa6e07f19d6b7694354e4b73a47104bf950ba
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-10-01 01:57:36 +0300

    add google fonts and make inter font is default font in App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mini_chat/features/splash_screen/presentation/screen/splash_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +10,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.interTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       title: "Mini What's App ",
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
