88c096f08a74bff33890b57ec575517fbc8651fa
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-09-30 19:54:34 +0300

    finish splash Screen

--- lib/features/splash_screen/presentation/screen/splash_screen.dart
+++ lib/features/splash_screen/presentation/screen/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mini_chat/core/constants/icons.dart';
 import 'package:mini_chat/features/home/presentation/screens/home.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -22,11 +24,27 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [],
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: SvgPicture.asset(
+                AppIcon.whatsApp,
+                height: MediaQuery.of(context).size.height * 0.2,
+                width: MediaQuery.of(context).size.width * 0.2,
+              ),
+            ),
+            const Text(
+              'Whats Up',
+              style: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
