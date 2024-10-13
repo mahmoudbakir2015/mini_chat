25f4eef18e7480aa3ce002bb10305b925ad3449f
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-10-13 22:06:02 +0300

    finish cirular avatar of profile

--- lib/features/profile/presentation/views/profile.dart
+++ lib/features/profile/presentation/views/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mini_chat/core/constants/app_assets.dart';
+import 'package:mini_chat/core/widget/build_stack_avatar.dart';
 import 'package:mini_chat/features/profile/presentation/views/widgets/profile_app_bar.dart';
 
 class Profile extends StatelessWidget {
@@ -8,6 +10,20 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildProfileAppBar(context),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: buildStackAvatar(
+                image: AppAssets.imageTest,
+                isProfile: true,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
