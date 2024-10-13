4dc856ac2b3b88329eb5822e0ebda32e26ab4064
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-10-13 22:28:18 +0300

    finish profile form

--- lib/features/profile/presentation/views/profile.dart
+++ lib/features/profile/presentation/views/profile.dart
@@ -2,26 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:mini_chat/core/constants/app_assets.dart';
 import 'package:mini_chat/core/widget/build_stack_avatar.dart';
 import 'package:mini_chat/features/profile/presentation/views/widgets/profile_app_bar.dart';
+import 'package:mini_chat/features/profile/presentation/views/widgets/profile_form.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController name = TextEditingController();
+
+    TextEditingController phone = TextEditingController();
     return Scaffold(
       appBar: buildProfileAppBar(context),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: buildStackAvatar(
-                image: AppAssets.imageTest,
-                isProfile: true,
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: buildStackAvatar(
+                  image: AppAssets.imageTest,
+                  isProfile: true,
+                ),
               ),
-            ),
-          ],
+              buildProfileForm(name, phone),
+            ],
+          ),
         ),
       ),
     );
