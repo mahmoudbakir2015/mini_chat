04f3a9c9cc037debfdbf32cd582f7a69da3bdf11
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-10-14 19:55:22 +0300

    finish profile bottom sheet

--- lib/features/profile/presentation/views/profile.dart
+++ lib/features/profile/presentation/views/profile.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mini_chat/features/profile/presentation/views/profile_body.dart';
 import 'package:mini_chat/features/profile/presentation/views/widgets/profile_app_bar.dart';
+// ignore: depend_on_referenced_packages
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({super.key});
 
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     TextEditingController name = TextEditingController();
@@ -12,7 +18,10 @@ class Profile extends StatelessWidget {
     TextEditingController phone = TextEditingController();
     return Scaffold(
       appBar: buildProfileAppBar(context),
-      body: buildProfileBody(name, phone, context),
+      body: ProfileBody(
+        name: name,
+        phone: phone,
+      ),
     );
   }
 }
--- lib/features/profile/presentation/views/profile_body.dart
+++ lib/features/profile/presentation/views/profile_body.dart
@@ -1,33 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:mini_chat/core/constants/app_assets.dart';
 import 'package:mini_chat/core/widget/build_stack_avatar.dart';
+import 'package:mini_chat/features/profile/presentation/views/widgets/profile_bottom_sheet.dart';
 import 'package:mini_chat/features/profile/presentation/views/widgets/profile_form.dart';
 import 'package:mini_chat/features/profile/presentation/views/widgets/profile_save_button.dart';
 
-SingleChildScrollView buildProfileBody(TextEditingController name,
-    TextEditingController phone, BuildContext context) {
-  return SingleChildScrollView(
-    child: Padding(
-      padding: const EdgeInsets.all(30),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: buildStackAvatar(
-              image: AppAssets.imageTest,
-              isProfile: true,
+class ProfileBody extends StatefulWidget {
+  final TextEditingController name;
+  final TextEditingController phone;
+  const ProfileBody({super.key, required this.name, required this.phone});
+
+  @override
+  State<ProfileBody> createState() => _ProfileBodyState();
+}
+
+class _ProfileBodyState extends State<ProfileBody> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: InkWell(
+                onTap: () {
+                  Scaffold.of(context).showBottomSheet(
+                    (context) => const ProfileBottomSheet(),
+                  );
+                },
+                child: buildStackAvatar(
+                  image: AppAssets.imageTest,
+                  isProfile: true,
+                ),
+              ),
             ),
-          ),
-          buildProfileForm(name, phone),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 30,
+            buildProfileForm(widget.name, widget.phone),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 30,
+              ),
+              child: buildSaveProfileButton(context),
             ),
-            child: buildSaveProfileButton(context),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
