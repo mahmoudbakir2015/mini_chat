490ff991994055de28de3b02974072d47015c92e
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-10-16 17:45:35 +0300

    add bloc to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:mini_chat/core/utils/observer.dart';
 import 'package:mini_chat/features/profile/presentation/views/profile.dart';
 // ignore: depend_on_referenced_packages
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
 
