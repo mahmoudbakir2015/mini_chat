93f2c2ccc7a21054762f2f3962ff68b29e3fe892
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-11-10 17:28:57 +0200

    add auth cubit

--- lib/features/auth/data/view_model/auth_cubit.dart
+++ lib/features/auth/data/view_model/auth_cubit.dart
@@ -1,6 +1,7 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mini_chat/features/auth/data/view_model/auth_states.dart';
 
 class AuthCubit extends Cubit<AuthStates> {
   AuthCubit() : super(AuthInitial());
+  static AuthCubit get(context) => BlocProvider.of(context);
 }
